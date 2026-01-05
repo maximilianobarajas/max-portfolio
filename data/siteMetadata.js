59170b3d16fa0d4d9f710119417fc0115cab54e1
Maximiliano Barajas <maxbarajas027@gmail.com>
2026-01-05 17:29:26 +0100

    update newsletter

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,17 +6,21 @@ const siteMetadata = {
     'Aspiring to leverage expertise in Mathematics and Computer Science for scientific computing, machine learning, and advanced data modeling.',
   language: 'en-us',
   theme: 'dark', // System, dark or light
-  siteUrl: 'https://maxbarajas.com', // Tu dominio futuro
+  siteUrl: 'https://maxbarajas.com',
   siteRepo: 'https://github.com/maximilianobarajas/max-portfolio',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
   email: 'maxbarajas027@gmail.com',
-  github: 'https://github.com/maximilianobarajas', // Asumiendo este usuario
+  github: 'https://github.com/maximilianobarajas',
   linkedin: 'https://www.linkedin.com/in/max-barajas',
   locale: 'en-US',
   analytics: {
-    googleAnalyticsId: '', // Puedes ponerlo luego
+    googleAnalyticsId: '', 
+  },
+  // 👇 AGREGAR ESTO ES VITAL PARA QUE NO TRUENE 👇
+  newsletter: {
+    provider: '', // Dejar vacío evita que el código busque claves API
   },
 }
 
